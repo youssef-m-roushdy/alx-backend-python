3f7c1c053ea25103e28bc979b0a5e80a8abeef06
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-08-01 21:55:37 +0300

    .

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+    Concatenate two strings and return the result.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenate two strings and return the result.
+
+    Parameters:
+    str1 (str): The first string.
+    str2 (str): The second string.
+
+    Returns:
+    str: The concatenation of str1 and str2.
+    """
+    return str1 + str2
