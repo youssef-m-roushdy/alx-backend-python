77675347ff7b094c1e465890c73128ac28a19f23
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-08-01 22:54:16 +0300

    .

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+    Calculate the sum of a list of float numbers.
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    Calculate the sum of a list of float numbers.
+
+    Parameters:
+    input_list (List[float]): The list of float numbers to sum.
+    
+    Returns:
+    float: The sum of the numbers in the list.
+    """
+    return sum(input_list)
