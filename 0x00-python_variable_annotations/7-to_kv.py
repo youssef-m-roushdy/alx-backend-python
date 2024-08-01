0b8783a65d83bce364c5763b14c548d89c69b452
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-08-01 23:15:03 +0300

    .

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,20 +1,8 @@
 #!/usr/bin/env python3
-"""
-    Create a tuple containing a string and a number (integer or float)
-"""
+"""Type-annotated function to_kv"""
 from typing import Tuple, Union
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, Union[int, float]]:
-    """
-    Create a tuple containing a string and a number (integer or float).
-
-    Parameters:
-    k (str): The string to include in the tuple.
-    v (Union[int, float]): The number (integer or float)
-    to include in the tuple.
-
-    Returns:
-    Tuple[str, Union[int, float]]: A tuple with the string and number.
-    """
+    """Returns tuple of a string and a float"""
     return (k, v ** 2)
