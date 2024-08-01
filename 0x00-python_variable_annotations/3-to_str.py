6e20e04ea7b6fc7761b3e13c017762b24f1d08da
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-08-01 22:19:55 +0300

    .

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+    Convert a float number to its string representation.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Convert a float number to its string representation.
+
+    Parameters:
+    n (float): The number to convert.
+
+    Returns:
+    str: The string representation of n.
+    """
+    return str(n)
