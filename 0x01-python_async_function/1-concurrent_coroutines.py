efe78b6798a0c8a968b296ee2ccd234280f45921
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-08-05 21:13:28 +0300

    .

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,6 +3,7 @@
     This module defines the wait_n function.
 """
 
+import heapq
 import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
@@ -12,5 +13,14 @@ async def wait_n(n: int, max_delay: int) -> list[float]:
         Executes wait_random multiple times and returns the results.
     """
     tasks = [wait_random(max_delay) for _ in range(n)]
-    results = await asyncio.gather(*tasks)
+    results = []
+    heap = []
+
+    for task in asyncio.as_completed(tasks):
+        result = await task
+        heapq.heappush(heap, result)
+
+    while heap:
+        results.append(heapq.heappop(heap))
+
     return results
