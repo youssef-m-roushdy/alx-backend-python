8e1543700905901092aecf9c61753ee082a7a84f
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-08-06 19:10:44 +0300

    .

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,7 +8,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator[float, None, None]: # type: ignore
+async def async_generator() -> Generator[float, None, None]:  # type: ignore
     """
     Asynchronously generates 10 random numbers between 0 and 10,
     with a 1-second delay between each number.
