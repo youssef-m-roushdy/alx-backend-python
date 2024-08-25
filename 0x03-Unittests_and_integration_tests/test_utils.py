34eaf915c2d97e4a886864fa4f57ef3b05d71624
youssef-m-roushdy <yossefmahmoud442004@gmail.com>
2024-08-25 20:55:55 +0300

    .

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -9,16 +9,15 @@ access_nested_map = __import__("utils").access_nested_map
 
 class TestAccessNestedMap(TestCase):
     @parameterized.expand([
-        ("test1", {"a": 1}, ("a",), 1),
-        ("test2", {"a": {"b": 2}}, ("a",), {"b": 2}),
-        ("test3", {"a": {"b": 2}}, ("a", "b"), 2)
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(self, name, nested_map, path, expected):
+    def test_access_nested_map(self, nested_map, path, expected):
         """
         Test the access_nested_map function with different inputs.
 
         Args:
-            name (str): The name of the test case.
             nested_map (dict): The nested dictionary to access.
             path (tuple): The keys path to access in the nested map.
             expected (Any): The expected result from the function.
